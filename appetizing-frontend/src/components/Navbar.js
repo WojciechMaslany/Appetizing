04b8912144827107a70f56d8c1000b976353e9b1
Wojciech Maślany <wojciech.maslany1@gmail.com>
2022-05-17 01:49:23 +0200

    Frontend project initial commit

---
+++ appetizing-frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <div className="navbar container">
+            <a href="#!">A<span>pp</span>etizing</a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">Settings</a>
+            </div>
+        </div>
+    )
+}
