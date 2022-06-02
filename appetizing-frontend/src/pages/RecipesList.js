b8183912c5efdc215caca93772df4749dc892944
Wojciech Maślany <wojciech.maslany1@gmail.com>
2022-06-02 23:30:46 +0200

    Recipes list searchbar and previous searches section

--- appetizing-frontend/src/pages/RecipesList.js
+++ appetizing-frontend/src/pages/RecipesList.js
@@ -1,7 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+
 export default function RecipesList() {
+
+    const searches = ['pizza', 'burger', 'cookies', 'juice', 'biriyani', 'salad', 'ice cream', 'lasagna', 'pudding', 'soup']
+
     return (
         <div>
-            RecipesList Page
+            <div className="previous-searches">
+                <h2>Previous searches</h2>
+                <div className="previous-searches-container">
+                    { searches.map((search, index) => (
+                        <div key={index} style={{animationDelay: index * .1 + "s"}} className="search-item">
+                            {search}
+                        </div>
+                    ))}
+                </div>
+                <div className="search-box">
+                    <input type="text" placeholder="Search..."/>
+                    <button className="btn">
+                        <FontAwesomeIcon icon={faSearch}/>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
